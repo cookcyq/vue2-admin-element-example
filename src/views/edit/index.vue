eeb0c7d78bea050f15ac5381caf87d02d7f34ecc
cookcyq <1499359024@qq.com>
2019-05-20 22:54:18 +0800

    personal setting

---
+++ src/views/edit/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="edit">
+    <ed-upload></ed-upload>
+    <ed-form></ed-form>
+  </div>
+</template>
+<script>
+import upload from '@/views/edit/components/upload.vue'
+import form from '@/views/edit/components/form.vue'
+export default {
+  components: { 'ed-form': form, 'ed-upload': upload }
+}
+</script>
