b0388025d95f46204faaffb63685ece1a949056a
cookcyq <1499359024@qq.com>
2019-05-20 22:48:53 +0800

    main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/layout/css/login1.css'
 
+import App from '@/App.vue'
+import router from '@/router/router.js'
+import store from '@/store/store.js'
+// import axios from 'axios'
+// Vue.prototype.axios = axios
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
