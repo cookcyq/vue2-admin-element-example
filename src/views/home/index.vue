129a340dd46a9b1763a6f2747189a2fe46725b17
cookcyq <1499359024@qq.com>
2019-05-20 22:53:55 +0800

    home index

---
+++ src/views/home/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+import { home } from '@/api/home.js'
+export default {
+  data: function() {
+    return {
+      title: ''
+    }
+  },
+  created: function() {
+    this.gethome()
+  },
+  methods: {
+    gethome: function() {
+      home()
+        .then(res => {
+          this.title = res.data.data.content
+        })
+        .catch(error => {
+          console.log('请求错误' + error)
+        })
+    }
+  }
+}
+</script>
+
