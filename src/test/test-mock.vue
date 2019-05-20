c9ff1e9944307cac8d2329eefa1fae34bebee94c
cookcyq <1499359024@qq.com>
2019-05-20 22:55:10 +0800

    test mock views

---
+++ src/test/test-mock.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="testMck">
+    <button @click="mock">请求mock</button>
+  </div>
+</template>
+
+<script>
+import { all_article_get } from '../../mock/user/index.js'
+export default {
+  data: function() {
+    return {}
+  },
+  created: function() {},
+  methods: {
+    mock: function() {
+      all_article_get()
+        .then(res => {
+          console.log(res)
+        })
+        .catch(error => {
+          console.log('有错误' + error)
+        })
+    }
+  }
+}
+</script>
+
